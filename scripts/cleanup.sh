79394a332698c2817644c58ce61abc00fed39886
shellclear <12013211+shellclear@users.noreply.github.com>
2026-05-10 15:02:44 +0200

    added new mirror container to cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-podman rm -f registry01-postgresql-quay registry01-redis-quay registry01-quay-registry registry02-postgresql-quay registry02-redis-quay registry02-quay-registry registry-quay-mirror
+podman rm -f registry01-postgresql-quay registry01-redis-quay registry01-quay-registry registry02-postgresql-quay registry02-redis-quay registry02-quay-registry registry01-quay-mirror registry02-quay-mirror
 podman volume prune -f
