47653f8fcd3a6a1aeeca66bdd808e781f7659da8
shellclear <12013211+shellclear@users.noreply.github.com>
2026-05-11 16:50:36 +0200

    new volumes and containers

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-podman rm -f registry01-postgresql-quay registry01-redis-quay registry01-quay-registry registry02-postgresql-quay registry02-redis-quay registry02-quay-registry registry01-quay-mirror registry02-quay-mirror
+podman rm -f registry01-quay317-postgresql-quay \
+             registry01-quay317-redis-quay \
+             registry01-quay317-quay-registry \
+             registry02-quay317-postgresql-quay \
+             registry02-quay317-redis-quay \
+             registry02-quay317-quay-registry \
+             registry01-quay317-quay-mirror \
+             registry02-quay317-quay-mirror \
+             registry01-quay314-postgresql-quay \
+             registry01-quay314-redis-quay \
+             registry01-quay314-quay-registry \
+             registry02-quay314-postgresql-quay \
+             registry02-quay314-redis-quay \
+             registry02-quay314-quay-registry \
+             registry01-quay314-quay-mirror \
+             registry02-quay314-quay-mirror
+
 podman volume prune -f
